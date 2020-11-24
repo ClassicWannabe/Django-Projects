66f6bbe72873ab4b4630182a0f5abfdffa063705
RuslanTheGuy <67461011+ruslan@users.noreply.github.com>
2020-11-24 13:48:42 +0500

    Change the project folder name

--- StarBlog/urls.py
+++ StarBlog/urls.py
@@ -1,4 +1,4 @@
-"""my_masterpiece URL Configuration
+"""StarBlog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
